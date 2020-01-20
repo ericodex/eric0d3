445cfa0efdae9d4decd25ab74ce2663abd43eba0
Ericódigos <eric.075315@gmail.com>
2020-01-20 15:46:10 -0300

    Animation widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'db_plantas.dart';
 import 'package:custom_splash/custom_splash.dart';
-
+import 'package:simple_animations/simple_animations.dart';
 import 'dart:core';
 
 void main() async => runApp(MeuAplicativo());
@@ -10,6 +10,79 @@ void main() async => runApp(MeuAplicativo());
 // Importante: Tudo é um Widget.
 
 Map<int, Widget> op = {1: PaginaInicial(), 2: PaginaInicial()};
+
+
+class Box extends StatelessWidget {
+  static final boxDecoration = BoxDecoration(
+      color: Colors.black,
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      boxShadow: [
+        BoxShadow(
+            color: Colors.black.withAlpha(60),
+            blurRadius: 5,
+            offset: Offset(0, 8),
+            spreadRadius: 2)
+      ]);
+
+  @override
+  Widget build(BuildContext context) {
+    return ControlledAnimation(
+      duration: Duration(milliseconds: 400),
+      tween: Tween(begin: 0.0, end: 80.0),
+      builder: (context, height) {
+        return ControlledAnimation(
+          duration: Duration(milliseconds: 1200),
+          delay: Duration(milliseconds: 500),
+          tween: Tween(begin: 2.0, end: 300.0),
+          builder: (context, width) {
+            return Container(
+              decoration: boxDecoration,
+              width: width,
+              height: height,
+              child: TypewriterText("  Ericódigos  "),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  isEnoughRoomForTypewriter(width) => width > 20;
+}
+
+class TypewriterText extends StatelessWidget {
+  static const TEXT_STYLE =
+      TextStyle(letterSpacing: 5, fontSize: 20, fontWeight: FontWeight.w600, color: Colors.green);
+
+  final String text;
+  TypewriterText(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return ControlledAnimation(
+        duration: Duration(milliseconds: 800),
+        delay: Duration(milliseconds: 800),
+        tween: IntTween(begin: 0, end: text.length),
+        builder: (context, textLength) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(text.substring(0, textLength), style: TEXT_STYLE),
+              ControlledAnimation(
+                playback: Playback.LOOP,
+                duration: Duration(milliseconds: 600),
+                tween: IntTween(begin: 0, end: 1),
+                builder: (context, oneOrZero) {
+                  return Opacity(
+                      opacity: oneOrZero == 1 ? 1.0 : 0.0,
+                      child: Text("_", style: TEXT_STYLE));
+                },
+              )
+            ],
+          );
+        });
+  }
+}
 
 class MeuAplicativo extends StatelessWidget {
   @override
@@ -25,7 +98,7 @@ class MeuAplicativo extends StatelessWidget {
         home: CustomSplash(
           imagePath: 'assets/ericodigosSplash.gif',
           backGroundColor: Colors.black,
-          animationEffect: 'zoom-out',
+          animationEffect: 'fade-in',
           logoSize: 200,
           home: PaginaInicial(),
           duration: 4000,
@@ -34,49 +107,46 @@ class MeuAplicativo extends StatelessWidget {
         ) // Widget Pagina Inicial
         );
   }
-}>
+}
+
 
 // Página inivcial
 class PaginaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ericódigos Flutter'),
-      ),
-      body: Center(
+      body: Container(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            //Primeiro botão
-            const SizedBox(height: 60),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => SegundaPagina()));
-              },
-              child:
-                  const Text('Dados OffLine', style: TextStyle(fontSize: 20)),
+            //Terceiro botão
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Box(),
             ),
+            //Segundo botão
             const SizedBox(height: 30),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => SegundaPagina()));
-              },
-              textColor: Colors.white,
-              padding: const EdgeInsets.all(0.0),
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: <Color>[
-                      Color.fromRGBO(21, 8, 23, 1.0),
-                      Color.fromRGBO(103, 39, 112, 0.8),
-                      Color.fromRGBO(143, 55, 156, 0.6),
-                    ],
+            Center(
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context) => SegundaPagina()));
+                },
+                textColor: Colors.white,
+                padding: const EdgeInsets.all(0.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Color.fromRGBO(21, 8, 23, 1.0),
+                        Color.fromRGBO(103, 39, 112, 0.8),
+                        Color.fromRGBO(143, 55, 156, 0.6),
+                      ],
+                    ),
                   ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: const Text('Gradient Button',
+                      style: TextStyle(fontSize: 20)),
                 ),
-                padding: const EdgeInsets.all(10.0),
-                child: const Text('Gradient Button',
-                    style: TextStyle(fontSize: 20)),
               ),
             ),
           ],
@@ -90,7 +160,6 @@ class SegundaPagina extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Segunda Página")),
         body: Center(
             child: RaisedButton(
           onPressed: () {
